5ab4606dc95fa015d9d7fa3fc0a30457c94090e6
Albert Chang <a89529294@hotmail.com>
2022-04-13 15:22:17 +0800

    left panel initial draft done

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -1,44 +1,106 @@
-import React, { useState } from "react";
-import { ChevronRight, Folder as FolderIcon } from "react-feather";
+import React, { useState, useContext, createContext } from "react";
+import { ChevronRight, ChevronDown, Folder as FolderIcon } from "react-feather";
 
-const size = 20;
-
-type Asset = {
-  type: "asset";
-  name: string;
-};
+//name of folders should be unique
 
 type Folder = {
-  type: "folder";
   name: string;
-  content: (Folder | Asset)[];
+  content: Folder[];
+};
+
+type FolderStatus = { name: string; open: boolean };
+
+const FolderRow = ({
+  folder,
+  folderStatusArray,
+  setFolderStatusArray,
+}: {
+  folder: Folder;
+  setFolderStatusArray: Function;
+  folderStatusArray: FolderStatus[];
+}) => {
+  const currentFolderStatus = folderStatusArray.find(
+    (folderStatus) => folderStatus.name === folder.name
+  );
+
+  const render = (content: Array<Folder>) => {
+    return (
+      <ul>
+        {content.map((item) => {
+          return (
+            <FolderRow
+              folder={item}
+              folderStatusArray={folderStatusArray}
+              setFolderStatusArray={setFolderStatusArray}
+              key={item.name}
+            />
+          );
+        })}
+      </ul>
+    );
+  };
+
+  return (
+    <li className="pl-4">
+      <div
+        className="flex gap-2 py-2 cursor-pointer hover:bg-slate-100"
+        onClick={(e) => {
+          setFolderStatusArray((folderStatusArray: FolderStatus[]) => {
+            console.log(
+              folderStatusArray.find((item) => item.name === folder.name)
+            );
+            return folderStatusArray.map((folderStatus) =>
+              folderStatus.name === folder.name
+                ? { ...folderStatus, open: !folderStatus.open }
+                : folderStatus
+            );
+          });
+        }}
+      >
+        {folder.content.length ? (
+          <ChevronRight size={20} />
+        ) : (
+          <div className="w-5" />
+        )}
+        <FolderIcon size={20} />
+        {folder.name}
+      </div>
+      {folder.content.length > 0 &&
+        currentFolderStatus?.open &&
+        render(folder.content)}
+    </li>
+  );
 };
 
 const LeftPanel = () => {
-  const [fileStructure, setFileStructure] = useState<Folder>({
-    type: "folder",
+  const [rootFolder, setRootFolder] = useState<Folder>({
     name: "Root Folder",
     content: [
-      { type: "folder", name: "Documents", content: [] },
-      { type: "asset", name: "diary.txt" },
+      {
+        name: "Documents",
+        content: [
+          { name: "Documents - 1", content: [] },
+          { name: "Documents - 2", content: [] },
+        ],
+      },
+      { name: "Music", content: [] },
     ],
   });
+  const [folderStatusArray, setFolderStatusArray] = useState<FolderStatus[]>([
+    { name: "Root Folder", open: false },
+    { name: "Documents", open: false },
+    { name: "Documents - 1", open: false },
+    { name: "Documents - 2", open: false },
+    { name: "Music", open: false },
+  ]);
 
   return (
     <ul>
-      <li className="py-2 px-4 flex gap-2 cursor-pointer hover:bg-slate-100">
-        <ChevronRight size={size} />
-        <FolderIcon size={size} />
-        {fileStructure.name}
-      </li>
-      <li>
-        <ul>
-          {fileStructure.content.map((item) => {
-            if (item.type === "folder") return <li>i am folder</li>;
-            return <li>i am asset</li>;
-          })}
-        </ul>
-      </li>
+      <FolderRow
+        folder={rootFolder}
+        folderStatusArray={folderStatusArray}
+        setFolderStatusArray={setFolderStatusArray}
+      />
     </ul>
   );
 };
